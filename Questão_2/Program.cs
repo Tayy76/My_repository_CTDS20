d32f6fcf5cce8a4623d39c69526d78372f1f7bc7
Tayy76 <gimeneztata02@gmail.com>
2025-09-30 17:20:30 -0300

    Add files via upload

---
+++ Questão_2/Program.cs
@@ -0,0 +1,10 @@
+﻿int numero;
+int resultado;
+
+Console.WriteLine("Digite um número, por favor.");
+
+numero = int.Parse(Console.ReadLine());
+
+resultado = numero - 1;
+
+Console.WriteLine($"O antecessor de {numero} é {resultado}");
