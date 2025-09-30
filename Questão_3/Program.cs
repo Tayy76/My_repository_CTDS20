47d993c9b30ef88c30cdd5f3cc961477863c410f
Tayy76 <gimeneztata02@gmail.com>
2025-09-30 17:20:55 -0300

    Add files via upload

---
+++ Questão_3/Program.cs
@@ -0,0 +1,11 @@
+﻿int Base, Altura;
+
+Console.WriteLine("Digite a base do retângulo.");
+Base = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a altura do retângulo.");
+Altura = int.Parse(Console.ReadLine());
+
+int area = Base * Altura;
+
+Console.WriteLine($"A área do retângulo é {area}");
