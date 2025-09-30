b85613d47e3785e2a39d31de1c956e9d768f3e78
Tayy76 <gimeneztata02@gmail.com>
2025-09-30 17:21:21 -0300

    Add files via upload

---
+++ Questão_4/Program.cs
@@ -0,0 +1,6 @@
+﻿int idade, data;
+
+Console.WriteLine("Qual ano que você nasceu?");
+idade = int.Parse(Console.ReadLine());
+data = (2025 - idade) * 365;
+Console.WriteLine($"Você tem {data} dias de vida.");
