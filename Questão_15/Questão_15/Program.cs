cd5a95b09b1826c97d8dc4aabe6589b1dca887ba
Tayy76 <gimeneztata02@gmail.com>
2025-10-02 17:19:37 -0300

    Add files via upload

---
+++ Questão_15/Questão_15/Program.cs
@@ -0,0 +1,13 @@
+﻿int N;
+int num = 0;
+int valor = 0;
+
+Console.WriteLine("Digite qualquer coisa.");
+N = int.Parse(Console.ReadLine());
+
+for (int i = 1000; i < 2000; i++)
+{
+    num++;
+    valor = num/11;
+}
+Console.WriteLine(valor);
