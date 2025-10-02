f2b5a63b033354ae7001e167e8024d79fe273e04
Tayy76 <gimeneztata02@gmail.com>
2025-10-02 14:35:59 -0300

    Add files via upload

---
+++ Questão_8/Questão_8/Program.cs
@@ -0,0 +1,13 @@
+﻿int valor;
+
+Console.WriteLine("Digite um número.");
+valor = int.Parse(Console.ReadLine());
+
+if (valor > 0)
+{
+    Console.WriteLine("Seu número é positivo.");
+}
+else
+{
+    Console.WriteLine("Seu número é negativo.");
+}
