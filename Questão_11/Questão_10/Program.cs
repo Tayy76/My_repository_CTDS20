e8937e4194237d2c60a6a7181c119dc13bdf7855
Tayy76 <gimeneztata02@gmail.com>
2025-10-02 17:11:10 -0300

    Add files via upload

---
+++ Questão_11/Questão_10/Program.cs
@@ -0,0 +1,16 @@
+﻿int ano;
+int data;
+
+Console.WriteLine("Em que ano você nasceu?");
+ano = int.Parse(Console.ReadLine());
+
+data = 2025 - ano;
+
+if  (data >= 18)
+{
+    Console.WriteLine("Você tem idade para votar.");
+}
+else
+{
+    Console.WriteLine("Você não tem idade para votar.");
+}
