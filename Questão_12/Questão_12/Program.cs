afd45a4a2a4bf27ca6774219b49577cd8e33df58
Tayy76 <gimeneztata02@gmail.com>
2025-10-02 17:11:42 -0300

    Add files via upload

---
+++ Questão_12/Questão_12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int valor;
+int valor2;
+
+Console.WriteLine("Digite o primeiro número.");
+valor = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo número.");
+valor2 = int.Parse(Console.ReadLine());
+
+if (valor > valor2)
+{
+    Console.WriteLine($"{valor} é maior que {valor2}");
+}
+else
+{
+    Console.WriteLine($"{valor2} é maior que {valor}");
+}
