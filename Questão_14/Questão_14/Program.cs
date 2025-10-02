5e38f9641f8dd3edb6876f13e2d6aaeaa2d27543
Tayy76 <gimeneztata02@gmail.com>
2025-10-02 17:12:35 -0300

    Add files via upload

---
+++ Questão_14/Questão_14/Program.cs
@@ -0,0 +1,13 @@
+﻿int N;
+int num = 0;
+int valor = 0;
+
+Console.WriteLine("Digite um valor.");
+N = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < N; i++)
+{
+    num++;
+    valor = num;
+    Console.WriteLine(valor);
+}
