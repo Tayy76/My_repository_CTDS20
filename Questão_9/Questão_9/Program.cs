97019f8a3e60c5f42ba007dff22a266c6ca3c95b
Tayy76 <gimeneztata02@gmail.com>
2025-10-02 14:36:36 -0300

    Add files via upload

---
+++ Questão_9/Questão_9/Program.cs
@@ -0,0 +1,16 @@
+﻿double apple;
+double pagar;
+
+Console.WriteLine("Você vai comprar quantas maçãs?");
+apple = double.Parse(Console.ReadLine());
+
+if (apple < 12)
+{
+    pagar = apple * 1.30;
+    Console.WriteLine($"Você irá pagar R${pagar:F2}");
+}
+else
+{
+    pagar = apple * 1.00;
+    Console.WriteLine($"Você irá pagar R${pagar:F2}");
+}
