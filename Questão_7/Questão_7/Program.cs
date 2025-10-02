45f9830cb0da965ffa9463f9af10394b8431ed9c
Tayy76 <gimeneztata02@gmail.com>
2025-10-02 14:35:23 -0300

    Add files via upload

---
+++ Questão_7/Questão_7/Program.cs
@@ -0,0 +1,13 @@
+﻿int num;
+
+Console.WriteLine("Digite um número.");
+num = int.Parse(Console.ReadLine());
+
+if (num > 10)
+{
+    Console.WriteLine("Seu número é maior que 10.");
+}
+else
+{
+    Console.WriteLine("Seu número não é maior 10.");
+}
