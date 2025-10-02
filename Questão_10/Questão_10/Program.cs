84d665521f00f253682ca84d0c9c13fb69b19827
Tayy76 <gimeneztata02@gmail.com>
2025-10-02 14:37:07 -0300

    Add files via upload

---
+++ Questão_10/Questão_10/Program.cs
@@ -0,0 +1,16 @@
+﻿double nota1;
+double nota2;
+
+Console.WriteLine("Digite a sua primeira nota.");
+nota1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite a sua segunda nota.");
+nota2 = double.Parse(Console.ReadLine());
+
+if (nota1 + nota2 > 6)
+{
+    Console.WriteLine("Você foi aprovado.");
+}
+else
+{
+    Console.WriteLine("Você foi reprovado.");
+}
