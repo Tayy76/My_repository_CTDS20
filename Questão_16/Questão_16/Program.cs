f282a65f82ed577af750f7a25e974cb0cdfd39bf
Tayy76 <gimeneztata02@gmail.com>
2025-10-07 14:06:59 -0300

    Add files via upload

---
+++ Questão_16/Questão_16/Program.cs
@@ -0,0 +1,11 @@
+﻿int n;
+int resultado = 0;
+
+Console.WriteLine("Digite um número e vamos dizer o fatorial dele.");
+n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < n; i++)
+{
+    resultado = (n - 1) * n;
+}
+Console.WriteLine(resultado);
