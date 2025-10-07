dec9fd7aa4bcba726eb3d272990779203ddc29b5
Tayy76 <gimeneztata02@gmail.com>
2025-10-07 16:01:43 -0300

    Add files via upload

---
+++ Questão_18/Questão_18/Program.cs
@@ -0,0 +1,17 @@
+﻿double num = 1;
+double media = 1;
+
+for (double i = 0; i < media; i++)
+{
+    if (num != 0)
+    {
+        Console.WriteLine("Digite um número.");
+        num = double.Parse(Console.ReadLine());
+    }
+
+    if (num != 0)
+    {
+        media = num / 2;
+        Console.WriteLine(media);
+    }
+}
