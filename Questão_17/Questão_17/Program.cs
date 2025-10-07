acbe6ad21fe6078b479cdf719ca67b08686dfb64
Tayy76 <gimeneztata02@gmail.com>
2025-10-07 16:01:13 -0300

    Add files via upload

---
+++ Questão_17/Questão_17/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+
+int n;
+int n2;
+int n3;
+int n4;
+int n5;
+int nega = 0;
+
+Console.WriteLine("Digite 5 números.");
+n = int.Parse(Console.ReadLine());
+n2 = int.Parse(Console.ReadLine());
+n3 = int.Parse(Console.ReadLine());
+n4 = int.Parse(Console.ReadLine());
+n5 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{n} {n2} {n3} {n4} {n5}");
+
+if  (n < 0)
+{
+    nega = nega + 1;
+}
+if (n2 < 0)
+{
+    nega = nega + 1;
+}
+if (n3 < 0)
+{
+    nega = nega + 1;
+}
+if (n4 < 0)
+{
+    nega = nega + 1;
+}
+if (n5 < 0)
+{
+    nega = nega + 1;
+}
+Console.WriteLine($"{nega} são negativos.");
